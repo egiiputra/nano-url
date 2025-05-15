5008401080c19e8a2ccad377a010b18a18d3ac0b
egiiputra <egiputra450@gmail.com>
2025-05-15 15:08:24 +0700

    add endpoint to get amount and list data

--- app/Http/Controllers/LinksApiController.php
+++ app/Http/Controllers/LinksApiController.php
@@ -4,15 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class LinksApiController extends Controller
 {
+    public function count()
+    {
+        return response()->json([
+            'data' => DB::scalar("select count(id) from links where user_id=?", [auth()->id()])
+        ]);
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $page = $request->input('page') ?? 1;
+        $pageSize = $request->input('pageSize') ?? 10;
+
+        $data = DB::select(
+            "select short_url, long_url, expired_at from links where user_id=? limit ? offset ?",
+            [auth()->id(), $pageSize, ($page-1) * $pageSize]
+        );
+        return response()->json($data);
     }
 
     /**
