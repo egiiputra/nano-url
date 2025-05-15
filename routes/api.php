a2632bf0bd641572f1db0ec4af8f8b34c61dd214
egiiputra <egiputra450@gmail.com>
2025-05-15 15:07:59 +0700

    setup routing for links endpoint api

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinksApiController;
 
 Route::prefix('api')->group(function () {
-    Route::controller(LinksApiController::class)->group(function () {
-        Route::get('/links', 'index');
-        // Route::post('/orders', 'store');
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::controller(LinksApiController::class)->group(function () {
+            Route::get('/links/count', 'count');
+            Route::get('/links', 'index');
+            // Route::post('/orders', 'store');
+        });
     });
-})->middleware('auth:sanctum');
+});
