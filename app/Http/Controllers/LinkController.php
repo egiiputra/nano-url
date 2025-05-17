e3b950ffc5ba7c3f1b4521f9b42c5855d17810d0
egiiputra <egiputra450@gmail.com>
2025-05-17 14:57:07 +0700

    add validation custom message

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -50,39 +50,42 @@ class LinkController extends Controller
         // Check if short url is unique, if not than it fail
         $request->validate([
             'short_url' => [
-                'required',
                 'string',
+                'required',
                 'max:100',
+                'regex:/^(\w\d_-)$/',
                 function (string $attribute, mixed $value, Closure $fail) {
                     // specifying for short url that not expired yet
-                    $url = DB::scalar("select short_url from links where short_url = ? where expired_at > > current_timestamp(0)", [$value]);
+                    $url = DB::scalar("select short_url from links where short_url = ? and expired_at > current_timestamp(0)", [$value]);
                     if (!is_null($url)) {
                         $fail("Short url is already used");
                     }
                 }
             ],
-            'long_url' => 'required|url|max:100'
+            'long_url' => [
+                'string',
+                'required',
+                'max:100',
+                'regex:/^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/',
+            ],
         ]);
 
-        // TODO: Validate make sure there is no '/', '&', '?' and whitespace
-
-        // TODO insert time stamp value into DB
+        // get time stamp value for expired field
         $expiredDate = date_add(
             new \DateTime(),
             new \DateInterval('P' . ($request->expired ?? 10) . 'D')
         )->format("Y-m-d H:i:s");
 
         // TODO: insert if short_url is not already used
-        $id = DB::scalar("select id from links where short_url = ? ", [$value]);
+        $id = DB::scalar("select id from links where short_url = ? ", [$request->short_url]);
         if (!is_null($id)) {
             // update if short_url is already used
             DB::update('update links set user_id = ?, long_url = ?, expired_at = ? where id = ?', [
-                $request->user_id,
+                auth()->id(),
                 $request->long_url,
                 $expiredDate,
                 $id,
             ]);
-
         }
         
         $res = DB::insert('insert into links (user_id, short_url, long_url, expired_at) values (?, ?, ?, ?)', [
