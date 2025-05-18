19ca74e399c3b7ac343370ec904c09757c45d2cb
egiiputra <egiputra450@gmail.com>
2025-05-18 22:18:02 +0700

    redirect to home after login and fix api route

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LinksApiController;
 
 Route::prefix('api')->group(function () {
-    Route::middleware('auth:sanctum')->group(function() {
-        Route::controller(LinksApiController::class)->group(function () {
-            Route::get('/links/count', 'count');
-            Route::get('/links', 'index');
-            // Route::post('/orders', 'store');
-        });
+    Route::controller(LinksApiController::class)->group(function () {
+        Route::get('/links/count', 'count');
+        Route::get('/links', 'index');
     });
 });
