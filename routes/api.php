33f9f1e7ace16a036924cc4c08b7815aedb13973
egiiputra <egiputra450@gmail.com>
2025-05-19 22:46:04 +0700

    add analytic route

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::prefix('api')->group(function () {
     Route::controller(LinksApiController::class)->group(function () {
         Route::get('/links/count', 'count');
         Route::get('/links', 'index');
+        Route::get('/links/{short_ur}/analytic', 'analytic');
     });
 });
